74ad0e66a09dea4e406b98e496e89ab3bb642ac5
eshwargundla@gmail.com <eshwargundla@gmail.com>
2025-07-17 05:17:50 +0000

    first3

--- resource.tf
+++ resource.tf
@@ -1,8 +1,8 @@
 resource "azurerm_resource_group" "resource1" {
   name     = "terra1-day3"
-  location = "East US 2"
+  location = "East US"
 }
 resource "azurerm_resource_group" "resource2" {
   name     = "terra2-day3"
-  location = "East US 2"
+  location = "East US"
 }
